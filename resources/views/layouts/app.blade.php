2003bedc0a92e9d826f597289792d18e5d8bc5c4
shada elkhoja <s.elkhoja@techlines.ly>
2019-12-16 17:42:34 +0200

    laravel_project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,36 +56,8 @@
       <span class="sr-only">Next</span>
     </a>
   </div>
-  
-  <div class="card-deck mt-5">
-    <div class="card w-100 h-100 !important">
-      <img src="mi.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">BLOOD DONATION ACTIVITY
-  </h5>
-        <p class="card-text">Blood Bank donation system can collect blood from many donators in short from various sources and distribute that blood to needy people who require blood..</p>
-      <br>
-      </div>
-    </div>
-    <br><br><br>
-  
-    <div class="card">
-      <img src="de.jpg" class="card-img-top mt-5" alt="...">
-      <div class="card-body">
-      <br><br><br><br><br><br><br>
-  
-        <h5 class="card-title">ABOUT US</h5>
-        <p class="card-text">make the process of obtaining blood from a blood bank hassle-free and corruption-free and make the system of blood bank management effective..</p>
-      </div>
-    </div>
-    <div class="card">
-      <img src="a1.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">CAMPS</h5>
-        <p class="card-text">The Blood Bank is fortunate to work with different Associations like Banks, Colleges, Insurance Institutes, Corporates, NGOs. Religious organization for conducting Blood Donation Camps throughout the year</p>
-      </div>
-    </div>
-    
+  <div class="container">
+    @yield('content')
   </div>
   <footer class="page-footer font-small blue pt-4">
     <div style="color:white" class="footer-copyright text-center py-3 bg-dark ">© 2019 Copyright:
